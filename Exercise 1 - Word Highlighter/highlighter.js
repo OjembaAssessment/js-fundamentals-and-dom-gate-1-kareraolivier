c5dd78073c166aebcf3cc5853d12910562acbd8f
kareraolivier <kareraolivier@gmail.com>
2023-07-24 11:24:44 +0200

    test

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,37 @@
 console.log("Exercise 1 - Word Highlighter");
+const Highlighter = () => {
+  let count = 0;
+  let newpal = {};
+  let pal = document
+    .getElementById("myParagraph")
+    .textContent.toString()
+    .split(" ")
+    .filter((text) => text != "");
+
+  console.log(pal.length);
+  obj = {};
+  for (let i = 0; i < pal.length; i++) {
+    if (obj[pal[i]] === undefined) {
+      obj[pal[i]] = 1;
+    } else {
+      obj[pal[i]]++;
+    }
+  }
+  console.log(obj);
+  let sortedValues = Object.keys(obj).sort(function (a, b) {
+    return obj[b] - obj[a];
+  });
+  let newcheck = sortedValues.slice(0, 5);
+  console.log(newcheck);
+  const newSpan = document.createElement("span");
+//   newSpan.style.backgroundColor = green;
+  for (let i = 0; i < pal.length; i++) {
+    for (let i = 0; i < newcheck.length; i++) {
+      if ((pal[i] = newcheck[i])) {
+        pal[i] = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk";
+      }
+    }
+  }
+  console.log(pal);
+};
+Highlighter();
--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,20 @@
 console.log("Exercise 3 - Timer");
+
+let nbr = 10;
+let x = setInterval(function () {
+  if (nbr >= 0) {
+    document.getElementById("demo").innerHTML = nbr;
+    nbr--;
+  }
+}, 1000);
+
+const addSec = (num) => {
+  nbr += num;
+};
+const removeSec = (num) => {
+  nbr -= num;
+};
+
+const disablestyle = () => {
+  document.getElementById("butto").setAttribute("style", "display:none");
+};
